02eb37304a444e72f8696eb8d987b1de8ad1c6e4
firak01 <firak01@t-online.de>
2015-02-11 11:06:51 +0100

    Weitere Beispiele bis zum Buch Seite 135

--- XSLTTransformation/src/xpath/XPathTest_322_01FGL.java
+++ XSLTTransformation/src/xpath/XPathTest_322_01FGL.java
@@ -22,15 +22,81 @@ public class XPathTest_322_01FGL {
 			System.out.println("File to process: '" + sFileName + "'");
 			
 			
-			//TODO GOON: Die Beispiele aus dem Buch hier anwenden
-			//+++ Nun das XPath - Ausdrücke, Merke: Groß-/Kleinschreibung ist wichtig.
+			//############Die Beispiele aus dem Buch hier anwenden (Seite 128)
+			//TODO: Der XPathParser müsste sich von einem Ergebnis zu einem anderen Bewegen können,
+			//      so dass ein Array von Expressions ausgwertet werden kann. Dabei sind nachfolgende Strings im Array diejenigen, die auf das vorherige Ergebnis angewendet werden.
+			//      Hintergrund: Momentan funktionieren von den Beispielen nur diejenigen, die vom Wurzelelement ausgehen!
 			
-			//Finde ausgehend vom Wurzelkontoen alle Erfolg Elemten im Dokument, beachte die beiden doppelten Doppelpunkte.
+			
+			//+++ Nun die XPath - Ausdrücke, Merke: Groß-/Kleinschreibung ist wichtig.
+			
+			//A) Kind Achse
+			
+			//Finde ausgehend vom Wurzelknoten alle Erfolg Elemten im Dokument, beachte die beiden doppelten Doppelpunkte.
 			String expression01 = "child::Erfolguebersicht/child::Erfolg";
 			
+			//Findet ausgehend vom Wurzelelement alle Gesamt-Elemente im Dokument in Dokumentenrichtung
+			String expression02 = "child::Erfolguebersicht/child::Erfolg/child::Gesamt";
+			
+			//Findet ausgehend vom Wurzelelement Gesamt-, Neukunde- und Erfolg-Elemente in Dokumentenrichtung
+			String expression03 = "child::Erfolguebersicht/child::Erfolg/following::*";
+			
+			//B) Geschwister Achse
+			
+			//findet ausgehend von einem Erfolg-Element die nachfolgenden Erfolg Geschwister 
+			String expression04 = "following-sibling::Erfolg"; //Als einzelner String liefert es kein sinnvolles Ergebnis. TODO: Klasse entsprechend anpassen. 
+			
+			//findet ausgehend von einem Erfolg-Element die vorhergehenden Erfolg Geschwister
+			String expression05 = "preceding-sibling::Erfolg";
+			
+			//findet ausgehend von einem Erfolg-Element das Erfolguebersicht-Element
+			String expression06 = "parent::*";
+			
+			//findet ausgend von einem Gesamt-Element sein Erfolg-Element als Elternelement und das Erfolguebersicht-Element
+			String expression07 = "ancestor::*";
+			
+			//C) Nachfahren Achse
+			
+			//findet ausgehend von einem Erfolg-Element die Elemente Gesamt und Neukunden in Dokumentenrichtung
+			String expression08 = "descendant::*";
+			
+			//findet ausgehend vom Wurzelelement alle Erfolg-Elemente und ihre Kinder
+			String expression09 = "descendant::*";
+			
+			//findet ausgehend vom Wurzelelement alle Gesamt-Elemente
+			String expression10 = "descendant::Gesamt";
 			
 			
-			String sExpression = expression01;
+			//D) Attribut Achse
+			
+			//findet ausgehend vom Wurzelelement alle Stadt Attribute
+			String expression11 = "descendant::Erfolg/attribute::Stadt";
+			
+			//findet ausgehend vom Wurzelelement alle Attribute in Erfolg
+			String expression12 = "descendant::Erfolg/attribute::*";
+			
+			//findet ausgehend von einem Erfolg-element das Attribut Monat in seinem Elternelement
+			String expression13 = "parent::Erfolg/attribute::Monat";
+			
+			//findet ausgehend von einem Erfolg-Element seine Attribute in Dokumentrichtung
+			String expression14 = "attribute::*";
+	
+			
+			//######################################
+			//Prädikate, stehen in eckigen Klammern, dienen als "Feinsteuerung" der knotenlokalisierung
+			//           und können ebenfalls wieder Pfade und Funktionen enthalten.
+			//Siehe Buch Seiten 133ff
+			//zu Abkürzungen, die hier verwendet werden, siehe buch Seite 129
+			
+			//Welche Erfolg-Elemente haben mehr als 10 Neukunden?
+			String expression15 = "//Erfolg[Neukunden>10]"; //Merke: // ist eine Abkürzung für /descendant-or-self::node()
+			
+			//Welche Städte in welchem Monat haben mehr als 10 Neukunden?
+			String expression16 = "//Erfolg[Neukunden>10]/attribute::*";
+			
+			//Todo GOON: Weitere Beispiele aus dem Buch Seite 135ff
+			
+			String sExpression = expression16;
 			
 			//+++++++++++++++++++++++++++++++++++++++++++++++++++
 			String[] argsForParser = new String[3];
